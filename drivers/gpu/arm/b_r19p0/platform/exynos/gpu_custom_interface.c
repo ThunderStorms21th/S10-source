0adc6c8f72c90f6ab6190bf1f06930da6e337725
ThunderStorms21th <pinakastorm@gmail.com>
2020-12-27 18:31:33 +0100

    drivers/gpu/b_r19p0: change max freq from 806 to 754 MHz
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/gpu/arm/b_r19p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/b_r19p0/platform/exynos/gpu_custom_interface.c
@@ -2074,7 +2074,7 @@ static ssize_t set_kernel_sysfs_user_max_clock(struct kobject *kobj, struct kobj
 
 		if (clock == 100000 || clock == 156000 || clock == 200000 || clock == 260000 || clock == 325000
                 || clock == 377000 || clock == 433000 || clock == 572000 || clock == 650000 || clock == 702000
-                || clock == 806000) {
+                || clock == 754000) {
 
 			platform->gpu_max_clock = clock;
 		} else {
@@ -2107,7 +2107,7 @@ static ssize_t set_kernel_sysfs_user_min_clock(struct kobject *kobj, struct kobj
 
 		if (clock == 100000 || clock == 156000 || clock == 200000 || clock == 260000 || clock == 325000
                 || clock == 377000 || clock == 433000 || clock == 572000 || clock == 650000 || clock == 702000
-                || clock == 806000) {
+                || clock == 754000) {
 
 			platform->gpu_min_clock = clock;
 		} else {
