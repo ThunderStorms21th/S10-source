7c93b0c35c8ec5079b791c3b703f11bc7a4b278d
ThunderStorms21th <pinakastorm@gmail.com>
2021-01-16 23:32:03 +0100

    ts build scripts: update to TS v1.6.2
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- ts-build.sh
+++ ts-build.sh
@@ -5,7 +5,7 @@
 
 LOG=compile_build.log
 RDIR=$(pwd)
-export K_VERSION="v1.4"
+export K_VERSION="v1.6"
 export K_NAME="ThundeRStormS-Kernel"
 export K_BASE="DTJA"
 
@@ -94,8 +94,8 @@ RUN_PROGRAM()
     # BUILD_DTBO
     # BUILD_DTB
     cp -f boot-$MODEL.img builds/$K_NAME-$K_BASE-OneUI-Q-$MODEL-$K_VERSION.img
-    cp -f $MODEL-dtb.img builds/dtb-dtbo/$MODEL-dtb.img
-    cp -f $MODEL-dtbo.img builds/dtb-dtbo/$MODEL-dtbo.img
+    cp -f $MODEL-dtb.img builds/zip-OneUIQ/common/ts/dtb/$MODEL-dtb.img
+    cp -f $MODEL-dtbo.img builds/zip-OneUIQ/common/ts/dtb/$MODEL-dtbo.img
 }
 
 RUN_PROGRAM2()
@@ -104,8 +104,8 @@ RUN_PROGRAM2()
     # BUILD_DTBO
     # BUILD_DTB
     cp -f boot-$MODEL.img builds/$K_NAME-$K_BASE-OneUI-Q-$MODEL-$K_VERSION.img
-    cp -f $MODEL-dtb.img builds/dtb-dtbo/$MODEL-dtb.img
-    cp -f $MODEL-dtbo.img builds/dtb-dtbo/$MODEL-dtbo.img
+    cp -f $MODEL-dtb.img builds/zip-OneUIQ/common/ts/dtb/$MODEL-dtb.img
+    cp -f $MODEL-dtbo.img builds/zip-OneUIQ/common/ts/dtb/$MODEL-dtbo.img
 }
 
 BUILD_DTBO()
