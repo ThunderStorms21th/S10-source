91f11d572a06343fa1a4a0b82236970b887f3d32
ThunderStorms21th <pinakastorm@gmail.com>
2021-02-12 13:20:37 +0100

    sched: fair: Use Google's schedtune_margin implementation
    https://android.googlesource.com/kernel/msm/+/7a1b8ea81e64234cc2e9bff7634eb88d755d715b
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- kernel/sched/ems/multi_load.c
+++ kernel/sched/ems/multi_load.c
@@ -35,14 +35,11 @@ unsigned long ml_task_runnable(struct task_struct *p)
 {
 	int boost = schedtune_task_boost(p);
 	unsigned long runnable_avg = READ_ONCE(p->se.avg.ml.runnable_avg);
-	unsigned long capacity;
 
 	if (boost == 0)
 		return runnable_avg;
 
-	capacity = capacity_orig_of_sse(task_cpu(p), p->sse);
-
-	return runnable_avg + schedtune_margin(capacity, runnable_avg, boost);
+	return runnable_avg + schedtune_margin(SCHED_CAPACITY_SCALE, runnable_avg, boost);;
 }
 
 /*
@@ -86,14 +83,11 @@ unsigned long ml_boosted_task_util(struct task_struct *p)
 {
 	int boost = schedtune_task_boost(p);
 	unsigned long util = ml_task_util(p);
-	unsigned long capacity;
 
 	if (boost == 0)
 		return util;
 
-	capacity = capacity_orig_of_sse(task_cpu(p), p->sse);
-
-	return util + schedtune_margin(capacity, util, boost);
+	return util + schedtune_margin(SCHED_CAPACITY_SCALE, util, boost);
 }
 
 /*
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6583,8 +6583,10 @@ schedtune_margin(unsigned long capacity, unsigned long signal, long boost)
 	 * The obtained M could be used by the caller to "boost" S.
 	 */
 	if (boost >= 0) {
-		margin  = capacity - signal;
-		margin *= boost;
+		if (capacity > signal) {
+			margin  = capacity - signal;
+			margin *= boost;
+		}
 	} else
 		margin = -signal * boost;
 
@@ -6613,15 +6615,13 @@ static inline long
 schedtune_task_margin(struct task_struct *task)
 {
 	int boost = schedtune_task_boost(task);
-	unsigned long util, capacity;
+	unsigned long util;
 
 	if (boost == 0)
 		return 0;
 
 	util = task_util_est(task);
-	capacity = capacity_orig_of(task_cpu(task));
-
-	return schedtune_margin(capacity, util, boost);
+	return schedtune_margin(SCHED_CAPACITY_SCALE, util, boost);
 }
 
 #else /* CONFIG_SCHED_TUNE */
