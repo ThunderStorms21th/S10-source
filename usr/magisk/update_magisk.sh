4a73b6fb6ec8cec681295b994d96071c08cd7537
ThunderStorms21th <pinakastorm@gmail.com>
2022-01-11 13:08:29 +0100

    Add +magisk=alpha Signed-off-by: Denis Efremov <efremov@linux.com>
    Thanks to Denis for this commit : https://github.com/CruelKernel/samsung-exynos9820/commit/2e4d98198a0f4a043d1c3d586f784e836591ba71
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,6 +9,10 @@ if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
 	magisk_link="https://github.com/topjohnwu/magisk-files/raw/${nver}/app-debug.apk"
+elif [ "x$1" = "xalpha" ]
+then
+	nver="alpha"
+	magisk_link="https://github.com/vvb2060/magisk_files/raw/${nver}/app-release.apk"
 else
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
@@ -17,7 +21,7 @@ else
 	fi
 	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk-${nver}.apk"
 fi
-if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/magiskinit" \) -o \( "$nver" = "canary" \) ]
+if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/magiskinit" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "alpha" \) ]
 then
 	echo "Updating Magisk from $ver to $nver"
 	curl -s --output "$DIR/magisk.zip" -L "$magisk_link"
